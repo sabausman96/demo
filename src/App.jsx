7f04b1cd7b6fd50e1d77e6cacf2a5a9046082b91
sabausman96 <sabausman196@protonmail.com>
2026-08-18 19:24:21 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <p>🚀 Pushed via Terminal (git push) — testing the full command-line workflow!</p>
       <p>I am pushing the changes my self</p>
       <p>testing this with another editor</p>
-      <h3>this is heading i am testing<h3/>
+      <h3>this is heading i am testing</h3>
     </div>
   );
 }
